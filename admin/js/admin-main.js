5bd1f3dc2b9d6fb599809184df5589766fd8d148
isaeyeon <igarcia@eyeonresponse.com>
2014-04-02 13:27:38 -0400

    added the logout and a simple modal for adding an event

--- admin/js/admin-main.js
+++ admin/js/admin-main.js
@@ -13,3 +13,54 @@ function notLoggedIn(){
 	$('#addEventLi').hide();
 	$('#eventLi').hide();
 }
+
+function signIn(){
+	if($('#signinUs').val()==""){
+      	$('#signinEr').text("Name is required");
+      	$('#signinUs').addClass('focusedInput').focus();
+    } else if ($('#signinPw').val()==""){
+    	$('#signinEr').text("Password is required");
+      	$('#signinPw').addClass('focusedInput').focus();
+    } else {
+    	Parse.User.logIn($('#signinUs').val(), $('#signinPw').val(), {
+		  success: function(user) {
+		    $('#signinUs').val("");
+			$('#signinPw').val("");
+			$('#signinForm').modal('hide');
+			loggedIn();
+		  },
+		  error: function(user, error) {
+		    console.log("There was an error signing in");
+		  }
+		});
+    }
+}
+
+function signUp(){
+	if($('#signupUs').val()==""){
+      	$('#signupEr').text("Name is required");
+      	$('#signupUs').addClass('focusedInput').focus();
+    } else if ($('#signupPw').val()==""){
+    	$('#signupEr').text("Password is required");
+      	$('#signupPw').addClass('focusedInput').focus();
+    } else if ($('#signupPw').val()!=$('#signupPwVe').val()) {
+    	$('#signupEr').text("Password does not match");
+      	$('#signupPwVe').addClass('focusedInput').focus();
+    } else {
+    	var user = new Parse.User();
+		user.set("username", $('#signupUs').val());
+		user.set("password", $('#signupPw').val());
+		user.signUp(null, {
+			success: function(u){
+				$('#signupUs').val("");
+				$('#signupPw').val("");
+				$('#signupPwVe').val("");
+				$('#signupForm').modal('hide');
+				loggedIn();
+			},
+			error: function(u, e){
+				console.log("There was an error signing up");
+			}
+		});
+    }
+}
