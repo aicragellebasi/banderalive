37bf48bd93d28e2acdac90d116864f0c84b73d10
isaeyeon <igarcia@eyeonresponse.com>
2014-04-03 17:05:36 -0400

    add event and list event functions, refresh event need to clear up previous list

--- admin/js/admin-main.js
+++ admin/js/admin-main.js
@@ -64,3 +64,70 @@ function signUp(){
 		});
     }
 }
+
+function addEvent(nEvent, data){
+	console.log(nEvent);
+	nEvent.set("title", data.eventTi);
+	nEvent.set("text", data.eventTe);
+
+	nEvent.save(null, {
+		success: function(e){
+			console.log(e.id);
+			console.log(e.get("title"));
+			console.log(e.get("text"));
+			$('#addEventEr').text("Thanks, your event has been saved.");
+			setTimeout(function(){
+				$('#addEventForm').modal('hide');
+			},1000);
+		},
+		error: function(e,i){
+			$('#addEventEr').text("There was a problem saving the event");
+		}
+	});	
+	
+}
+
+
+function listEvent(query){
+	query.find({
+		success: function(results){
+			//console.log('success retrieving the events');
+
+			for (i=0;i<results.length;i++){
+
+				var dataEvent = results[i];
+				var toggleID = 'collapse-'+i;
+				buildCollapsePanel(dataEvent, toggleID)
+
+			}
+		},
+		error: function(error){
+			console.log('error retrieving the events');
+		}
+	});	
+}
+
+
+function buildCollapsePanel(data, collapseID){
+	var panel = $('<div>').addClass('panel panel-default');
+
+	//BUILDING THE HEADERS FOR THE COLLAPSIBLE PANELS
+	var panelHeading = $('<div>').addClass('panel-heading');
+	var panelH4 = $('<h4>').addClass('panel-title');
+	var panelH4a = $('<a>').attr('data-toggle', 'collapse').attr('data-parent','#accordion').attr('href','#'+collapseID).append(data.get('title'));
+
+	panelH4.append(panelH4a);
+	panelHeading.append(panelH4);
+	panel.append(panelHeading);
+
+	//BUILDING THE CONTENT FOR THE COLLAPSIBLE PANELS
+	var panelContent = $('<div>').addClass('panel-collapse collapse').attr('id',collapseID);
+	var panelBody = $('<div>').addClass('panel-body').append(data.get('text'));
+
+	panelContent.append(panelBody);
+	panel.append(panelContent);
+
+	// FINALLY APPEND ALL OF THE CONTENT TO EACH PANEL
+	$('#accordion').append(panel);
+}
+
